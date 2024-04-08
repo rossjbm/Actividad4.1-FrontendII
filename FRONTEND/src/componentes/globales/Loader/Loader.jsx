efcad5c90bfbfd596b3047b66c2ff9f362d7995e
JMMP33 <94369965+JMMP33@users.noreply.github.com>
2024-04-08 13:00:45 -0400

    Se añade componente de loader

---
+++ FRONTEND/src/componentes/globales/Loader/Loader.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Loader = () => {
+  return (
+    <div className="flex flex-col justify-center items-center min-h-screen bg-marron-400 dark:bg-Verde-oscuro-400">
+      <div className="animate-spin rounded-full h-20 w-20  border-t-4 border-b-4 border-black dark:border-white"></div>
+      <p className="text-lg font-bold text-black dark:text-white mt-8">Cargando...</p>
+    </div>
+  );
+};
+
+export default Loader;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
