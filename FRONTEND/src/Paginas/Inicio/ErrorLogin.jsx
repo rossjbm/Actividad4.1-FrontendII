6838d8a54fd8d31a0e5b4bad584ce785a0cc7b6c
JMMP33 <94369965+JMMP33@users.noreply.github.com>
2024-04-09 11:40:56 -0400

    Colocando Link para inicio de sesion

--- FRONTEND/src/Paginas/Inicio/ErrorLogin.jsx
+++ FRONTEND/src/Paginas/Inicio/ErrorLogin.jsx
@@ -1,12 +1,15 @@
-function ErrorLogin() {
+import { Link } from 'react-router-dom';
 
+function ErrorLogin() {
     return (
-        <>
-            <main className="w-full h-[100vh] dark:bg-Verde-oscuro-800 bg-white flex justify-center items-center">
-                <h1 className="text-Verde-oscuro-800 dark:text-white text-3xl font-titulo">No has inicia sesion</h1>
-            </main>
-        </>
-    )
+        <main className="w-full h-[100vh] dark:bg-Verde-oscuro-800 bg-white flex flex-col justify-center items-center">
+            <div className="mb-4 text-center">
+                <h1 className="text-Verde-oscuro-800 p-8 dark:text-white text-3xl font-titulo">No has iniciado sesión</h1>
+                <Link to="/iniciosesion" className="hover:underline text-black dark:text-white">Inicia Sesión Aquí</Link>
+            </div>
+        </main>
+    );
 }
 
-export default ErrorLogin
+export default ErrorLogin;
+
