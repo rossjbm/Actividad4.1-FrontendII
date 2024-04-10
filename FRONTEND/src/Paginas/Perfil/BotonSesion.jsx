03c86601a914281e515301ebb9f30b66c4e164fc
JMMP33 <94369965+JMMP33@users.noreply.github.com>
2024-04-10 18:47:49 -0400

    Loader aqui

--- FRONTEND/src/Paginas/Perfil/BotonSesion.jsx
+++ FRONTEND/src/Paginas/Perfil/BotonSesion.jsx
@@ -1,8 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import Loader from "../../Componentes/globales/Loader/Loader";
 import { NavLink } from 'react-router-dom';
 import Perfil from './Perfil';
 
+
 export function CerrarSesion({ isLogin, setIsLogin }) {
-  
+  const [loaded, setLoaded] = useState(false);
+
   const cerrarSesion = (e) => {
     localStorage.removeItem("token");
     localStorage.removeItem("usuarioId");
@@ -10,8 +14,19 @@ export function CerrarSesion({ isLogin, setIsLogin }) {
     window.location.href = "http://localhost:5000/iniciosesion";
   }
 
+    useEffect(() => {
+        const timer = setTimeout(() => {
+          setLoaded(true);
+        }, 1000); // Tiempo en milisegundos para simular la carga
+      
+        return () => clearTimeout(timer); // Limpia el timer al desmontar el componente
+      }, []);
+
   return (
     <>
+    {!loaded ? (
+      <Loader /> // Muestra el loader mientras se simula la carga
+    ) : (
       <div className='flex flex-col items-center justify-center h-full p-48'>
         
         {isLogin ? (
@@ -21,9 +36,10 @@ export function CerrarSesion({ isLogin, setIsLogin }) {
         )}
         {!isLogin && <Perfil />}
       </div>
-    </>
-  );
-}
+      )}
+      </>
+      );
+  }
 
 export default CerrarSesion;
 
