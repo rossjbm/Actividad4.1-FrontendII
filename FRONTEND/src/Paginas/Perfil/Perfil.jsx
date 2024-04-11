7752dc49e9ebb9ba6d3f1d087a1179147769476e
JMMP33 <94369965+JMMP33@users.noreply.github.com>
2024-04-11 06:54:39 -0400

    Perfil con usuarios

--- FRONTEND/src/Paginas/Perfil/Perfil.jsx
+++ FRONTEND/src/Paginas/Perfil/Perfil.jsx
@@ -1,6 +1,33 @@
-//import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
+import { listarUsuarioId } from '../../peticiones/usuarios';
 
 const Perfil = () => {
+
+    const [usuarioId, setUsuarioId] = useState(localStorage.getItem("usuarioId"));
+    const [dataUsuario, setDataUsuario] = useState({});
+
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setLoaded(true);
+        }, 1000); // Tiempo en milisegundos para simular la carga
+
+        async function extraerDatos() {
+            try {
+                const data = await listarUsuarioId(usuarioId)
+                console.log('DATA USUARIO',data)
+                setDataUsuario(data)
+            } catch (error) {
+                console.log(error)
+                throw error
+            }
+
+        }
+        extraerDatos()
+        
+        return () => clearTimeout(timer); // Limpia el timer al desmontar el componente
+    }, []);
+
 
     return (
         <>
@@ -9,11 +36,11 @@ const Perfil = () => {
                 <h2 className="text-xl mb-4 text-center font-titulo dark:text-white">Perfil</h2>
 
                 <form className="space-y-4 ">
-                    <label htmlFor="nombreCompleto" className="block font-titulo dark:text-white">Nombre Completo:</label>
+                    <label htmlFor="nombreCompleto" className="block font-titulo dark:text-white">Nombre Completo: {dataUsuario.nombreCompleto} </label>
                     
-                    <label htmlFor="usuarioUnico" className="block font-titulo dark:text-white">Usuario:</label>
+                    <label htmlFor="usuarioUnico" className="block font-titulo dark:text-white">Usuario: {dataUsuario.usuarioUnico}</label>
 
-                    <label htmlFor="correo" className="block font-titulo dark:text-white">Correo:</label>
+                    <label htmlFor="correo" className="block font-titulo dark:text-white">Correo: {dataUsuario.correo}</label>
                 </form>                
             </div>
         </div>
